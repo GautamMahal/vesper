681c93eba58fa84bfc8e9d588498a2aefe078d3b
TrickeryJoker <TrickeryJoker@outlook.com>
2024-11-15 01:42:57 +0800

    init

---
+++ src/app/@chat/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div className={'flex justify-center items-center h-60 rounded-lg flex-1 bg-pink-300'}>
+            <h1>@Chat</h1>
+        </div>
+    );
+}
+
+export default Page;
---
+++ src/app/@work/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className={'flex justify-center items-center h-60 rounded-lg flex-1 bg-green-300'}>
+          <h1>@work</h1>
+        </div>
+    );
+};
+
+export default Page;
